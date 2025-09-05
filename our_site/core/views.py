35db0bb9547c4ef6fa282ed66f99368fcad113cc
Carlos Miranda <badleon2744@gmail.com>
2025-09-04 23:00:38 -0500

    Se añadió los nuevos views.
    Se añadió las views de los .html

--- our_site/core/views.py
+++ our_site/core/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, "manual/index.html")
+
+def control_versiones(request):
+    return render(request, "manual/control_versiones.html")
+
+def metodologias_agiles(request):
+    return render(request, "manual/metodologias_agiles.html")
+
+def documentacion(request):
+    return render(request, "manual/documentacion.html")
+
+def testing(request):
+    return render(request, "manual/testing.html")
+
+def trabajo_equipo(request):
+    return render(request, "manual/trabajo_equipo.html")
+
+def recursos(request):
+    return render(request, "manual/recursos.html")
